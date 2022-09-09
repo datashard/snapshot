1636db64551c7faf38b40c4bf06d921d3debe31c
Joshua <data@shard.wtf>
2022-09-09 14:50:44 +0200

    Two basic specs

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,24 @@
+/* eslint-env mocha */
+/* global cy */
+describe("@cypress/snapshot", () => {
+  context("simple types", () => {
+    it("works with objects", () => {
+      cy.fixture("File2").snapshot("Compare Files");
+    });
+
+    // it("works with numbers", () => {
+      // console.log(cy.wrap(42))
+      // cy.wrap(42).snapshot();
+    // });
+
+    // it("works with strings", () => {
+      // console.log(cy.wrap("foo-bar"))
+      // cy.wrap("foo-bar").snapshot();
+    // });
+
+    // it("works with arrays", () => {
+      // console.log(cy.wrap([1, 2, 3]))
+      // cy.wrap([1, 2, 3]).snapshot();
+    // });
+  });
+});

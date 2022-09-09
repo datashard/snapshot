62ac60e853cc1c277a23a48c0f5ea9cc5f338e93
Joshua <data@shard.wtf>
2022-09-09 14:32:12 +0200

    update to latest cypress version

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+const { functions } = require("./src/utils");
+
+
+module.exports = defineConfig({
+  snapshot: {
+    snapshotPath: "cypress/snapshots/",
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      functions.tasks(on, config);
+    },
+  },
+});

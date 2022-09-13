f10f29cf1d84c3aa324d248686c672660d1bc07d
Joshua <data@shard.wtf>
2022-09-13 16:03:57 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict'
 
 // global cy, Cypress
 
-const { functions } = require("./utils/index");
+const { functions } = require('./utils/index')
 
 module.exports = {
   register: functions.register,
   tasks: functions.tasks
-};
+}

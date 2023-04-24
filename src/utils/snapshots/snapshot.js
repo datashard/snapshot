0e861e92adde45c310b6be7025d4422e2cd5dabb
Joshua <data@shard.wtf>
2023-04-24 10:23:52 +0200

    use replaceAll instead to properly replace everything needed

--- src/utils/snapshots/snapshot.js
+++ src/utils/snapshots/snapshot.js
@@ -64,7 +64,7 @@ const set_snapshot = (
         };
 
         throw new Error(
-          `Snapshot Difference found.\nPlease Update the Snapshot\n\n${json.message.replace(' ', '&nbsp;')}`
+          `Snapshot Difference found.\nPlease Update the Snapshot\n\n${json.message.replaceAll(' ', '&nbsp;')}`
         );
       });
     }

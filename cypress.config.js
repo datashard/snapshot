7c6f997bc06ac5ac16b5be61023dd2068ba38f51
Joshua <data@shard.wtf>
2024-01-31 09:22:25 +0100

    redo config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   snapshot: {
-    // snapshotPath: "cypress/snapshots/",
-    // SNAPSHOT_UPDATE: true,
+    updateSnapshots: true,
     useFolders: true,
   },
 
